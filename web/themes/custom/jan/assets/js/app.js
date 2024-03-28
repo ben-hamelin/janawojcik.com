839417ee3c9abeca6ae77883ef9a0e62bfc5c977
Ben Hamelin <bhamelin@oomphinc.com>
2024-03-28 09:29:54 -0400

    Fix Safari scope bug

--- web/themes/custom/jan/assets/js/app.js
+++ web/themes/custom/jan/assets/js/app.js
@@ -177,67 +177,67 @@ const slideshow = document.querySelector('.slide-wrap');
 
 if (slideshow != null ) { //make sure we don't run this script if the slideshow is not present
 
-  let slides = document.querySelectorAll('.slide-entry'),
-slideCount = slides.length,
-currentSlide = 0,
-slideHeight = null,
-initialHeight = slides[0].clientHeight;
+  var slides = document.querySelectorAll('.slide-entry');
+  var slideCount = slides.length;
+  var currentSlide = 0;
+  var slideHeight = null;
+  var initialHeight = slides[0].clientHeight;
 
-slides[0].classList.add('active'); //on load, activate the first slide
+  slides[0].classList.add('active'); //on load, activate the first slide
 
-function moveToSlide(n) { // set up our slide navigation functionality
-  slides[currentSlide].className = 'slide-entry';
-  currentSlide = (n+slideCount)%slideCount;
-  slides[currentSlide].className = 'slide-entry active';
-  slideHeight = slides[currentSlide].clientHeight;
-  slideshow.style.height = slideHeight + 'px';
-  window.addEventListener('resize', function(){
-    resizedSlideHeight = slides[currentSlide].clientHeight;
-    slideshow.style.height = resizedSlideHeight + 'px';
-  });
-}
+    function moveToSlide(n) { // set up our slide navigation functionality
+      slides[currentSlide].className = 'slide-entry';
+      currentSlide = (n+slideCount)%slideCount;
+      slides[currentSlide].className = 'slide-entry active';
+      slideHeight = slides[currentSlide].clientHeight;
+      slideshow.style.height = slideHeight + 'px';
+      window.addEventListener('resize', function(){
+        resizedSlideHeight = slides[currentSlide].clientHeight;
+        slideshow.style.height = resizedSlideHeight + 'px';
+      });
+    }
 
-function nextSlide(e){
-  moveToSlide(currentSlide+1);
-  let code = e.keyCode;
-  if( code == 39 ) {
-    moveToSlide(currentSlide+1);
-  }
-};
-function prevSlide(e){
-  moveToSlide(currentSlide+-1);
-  let code = e.keyCode;
-  if( code == 37 ) {
-    moveToSlide(currentSlide+-1);
-  }
-};
+    function nextSlide(e){
+      moveToSlide(currentSlide+1);
+      var code = e.keyCode;
+      if( code == 39 ) {
+        moveToSlide(currentSlide+1);
+      }
+    };
+    function prevSlide(e){
+      moveToSlide(currentSlide+-1);
+      var code = e.keyCode;
+      if( code == 37 ) {
+        moveToSlide(currentSlide+-1);
+      }
+   };
 
-const slideHandlers = {
-  nextSlide: function(element){
-    document.querySelector(element).addEventListener('click',nextSlide);
-    document.body.addEventListener('keydown',nextSlide, false);
-  },
-  prevSlide: function(element){
-    document.querySelector(element).addEventListener('click',prevSlide);
-    document.body.addEventListener('keydown',prevSlide, false);
-  }
-}
+    const slideHandlers = {
+      nextSlide: function(element){
+        document.querySelector(element).addEventListener('click',nextSlide);
+        document.body.addEventListener('keydown',nextSlide, false);
+      },
+      prevSlide: function(element){
+        document.querySelector(element).addEventListener('click',prevSlide);
+        document.body.addEventListener('keydown',prevSlide, false);
+      }
+    }
 
-slideHandlers.nextSlide('#next-slide');
-slideHandlers.prevSlide('#prev-slide');
+    slideHandlers.nextSlide('#next-slide');
+    slideHandlers.prevSlide('#prev-slide');
 
 // Dynamic slideshow height
 
   slideshow.style.height = initialHeight + 'px'; //on load, set the height of the slider to the first active slide
 
-window.addEventListener('resize', function(){ // adjust the height of the slidehow as the browser is resized
-  let resizedHeight = slides[0].clientHeight;
-  slideshow.style.height = resizedHeight + 'px';
-});
+    window.addEventListener('resize', function(){ // adjust the height of the slidehow as the browser is resized
+      var resizedHeight = slides[0].clientHeight;
+      slideshow.style.height = resizedHeight + 'px';
+    });
 
 // Detect swipe events for touch devices, credit to Kirupa @ https://www.kirupa.com/html5/detecting_touch_swipe_gestures.htm
-let initialX = null;
-let initialY = null;
+var initialX = null;
+var initialY = null;
 function startTouch(e) {
   initialX = e.touches[0].clientX;
   initialY = e.touches[0].clientY;
@@ -249,30 +249,30 @@ function moveTouch(e) {
   if (initialY === null) {
     return;
   }
-  let currentX = e.touches[0].clientX;
-  let currentY = e.touches[0].clientY;
-  let diffX = initialX - currentX;
-  let diffY = initialY - currentY;
+  var currentX = e.touches[0].clientX;
+  var currentY = e.touches[0].clientY;
+  var diffX = initialX - currentX;
+  var diffY = initialY - currentY;
   if (Math.abs(diffX) > Math.abs(diffY)) {
     if (diffX > 0) {
-// swiped left
-moveToSlide(currentSlide+1);
-} else {
-// swiped right
-moveToSlide(currentSlide+-1);
-}
-}
-initialX = null;
-initialY = null;
-e.preventDefault();
-};
+    // swiped left
+    moveToSlide(currentSlide+1);
+    } else {
+    // swiped right
+    moveToSlide(currentSlide+-1);
+    }
+    }
+    initialX = null;
+    initialY = null;
+    e.preventDefault();
+    };
 
-slideshow.addEventListener("touchstart", startTouch, false);
-slideshow.addEventListener("touchmove", moveTouch, false);  
+    slideshow.addEventListener("touchstart", startTouch, false);
+    slideshow.addEventListener("touchmove", moveTouch, false);
 
-// optional autoplay function  
-  //setInterval(function(){
-    //nextSlide();
-  //},8000); 
+    // optional autoplay function
+      //setInterval(function(){
+        //nextSlide();
+      //},8000);
 
 } //end slideshow
